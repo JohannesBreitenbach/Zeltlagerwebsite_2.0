990190961c2f87fe8d2f8b8f890332415fb841e2
Johannes_Breitenbach <johannes.breitenbach@unibz.it>
2024-05-09 16:17:17 +0200

    removed trash files

--- Lab7/assignment/app.js
+++ Lab7/assignment/app.js
@@ -1,28 +1,36 @@
-const express = require('express')
+const express = require("express");
 
-const bodyParser = require('body-parser')
-const path = require('path')
+const bodyParser = require("body-parser");
+const path = require("path");
 
-const detailRoutes = require('./routes/book')
-const listRoutes = require('./routes/index')
-const { allowedNodeEnvironmentFlags } = require('process')
+const detailRoutes = require("./routes/book");
+const listRoutes = require("./routes/index");
+const { allowedNodeEnvironmentFlags } = require("process");
 
-const app = express()
+const app = express();
 
 app.locals.books = [
-		{isbn: '1', title: 'Book1', author: 'Markus Zanker',
-		 description: 'Lorem ipsum dolor sit amit', },
-		{isbn: '2', title: 'Book2', author: 'Max Mustermann',
-		 description: 'The new book of Max Mustermann', }
+  {
+    isbn: "1",
+    title: "Book1",
+    author: "Markus Zanker",
+    description: "Lorem ipsum dolor sit amit",
+  },
+  {
+    isbn: "2",
+    title: "Book2",
+    author: "Max Mustermann",
+    description: "The new book of Max Mustermann",
+  },
 ];
-	
-app.use(bodyParser.urlencoded({extended: true}))
 
-app.use('/book', detailRoutes)
-app.use(listRoutes)
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use("/book", detailRoutes);
+app.use(listRoutes);
 
 app.use((req, res, next) => {
-	res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
-})
+  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+});
 
-app.listen(3000) 
+app.listen(3000);
