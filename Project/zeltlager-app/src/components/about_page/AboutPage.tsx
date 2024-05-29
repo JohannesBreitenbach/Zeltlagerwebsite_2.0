864b0f7926c9ad2800db435fc92ecb3ff557544a
Johannes_Breitenbach <johannes.breitenbach@unibz.it>
2024-05-29 15:42:00 +0200

    added content component for about page

--- Project/zeltlager-app/src/components/about_page/AboutPage.tsx
+++ Project/zeltlager-app/src/components/about_page/AboutPage.tsx
@@ -3,7 +3,7 @@ import "./about.scss";
 
 function AboutPage() {
   return (
-    <section id="about-page" className="bg-light py-5">
+    <section id="about-page" className="bg-light py-5 w-100">
       <div className="container">
         <div className="row align-items-center">
           <div className="col-md-6 text-center mb-4 mb-md-0">
@@ -31,7 +31,21 @@ function AboutPage() {
           </div>
         </div>
       </div>
-      <ContentSection />
+      <ContentSection
+        heading="Wer organisiert das Zeltlager?"
+        content="Das Zeltlager wird von einer engagierten Gruppe von Freiwilligen und
+          erfahrenen Betreuern organisiert, die oft aus verschiedenen sozialen
+          und pädagogischen Bereichen stammen. Diese Organisatoren, die
+          möglicherweise Teil einer gemeinnützigen Organisation oder eines
+          Vereins sind, planen und koordinieren das gesamte Lager. Sie sorgen
+          für die Sicherheit der Teilnehmer, gestalten das Programm und kümmern
+          sich um die logistischen Details wie Verpflegung und Unterkunft. Die
+          Organisatoren sind meist auch in der Jugendarbeit erfahren und
+          verfügen über das notwendige Know-how, um den Teilnehmern eine
+          unvergessliche und lehrreiche Zeit zu bieten. Durch ihre Leidenschaft
+          und ihr Engagement wird das Zeltlager zu einem besonderen Erlebnis für
+          alle Beteiligten."
+      />
     </section>
   );
 }
